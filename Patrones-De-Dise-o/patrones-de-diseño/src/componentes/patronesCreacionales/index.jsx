98dfb33c29a8865826caf479da45dce22c2ae755
Cristhian Alejandro Cabezas <cacabezas36@ucatolica.edu.co>
2025-04-03 19:30:32 -0500

    ajustar carpetas

--- Patrones-De-Dise-o/patrones-de-diseño/src/componentes/patronesCreacionales/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-export { default as AbstractFactory } from "./AbstractFactory";
-export { default as Builder } from "./Builder";
-export { default as FactoryMethod } from "./FactoryMethod";
-export { default as Prototype } from "./Prototype";
-export { default as Singleton } from "./Singleton";
-export { default as Creacionales} from "./Creacionales"
--- Patrones-De-Dise-o/patrones-de-diseño/src/views/Patrones.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const patrones = () => {
-  return (
-    <div>patrones</div>
-  )
-}
-
-export default patrones
